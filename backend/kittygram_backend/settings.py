ac43c016cc523db76f73bcda45186149a2237dd6
Bog-VM <closedeyevisuals@yandex.ru>
2024-10-17 13:20:19 +0700

    settings backup check

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -6,11 +6,11 @@ from django.core.management.utils import get_random_secret_key
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
+SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
-DEBUG = os.getenv('DEBUG', '')
+DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(', ')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '158.160.31.226', 'kittygram.sytes.net']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
